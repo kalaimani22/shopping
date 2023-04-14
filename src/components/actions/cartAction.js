fdb87d3fbc507767916e69ef7deffe9a1f3660c7
Kalaimani <kalaimani22raja@gmail.com>
2023-04-14 16:59:51 +0530

    Action removed

--- src/components/actions/cartAction.js
+++ src/components/actions/cartAction.js
@@ -1,5 +1,5 @@
 
-import { ADD_TO_CART,REMOVE_ITEM,SUB_QUANTITY,ADD_QUANTITY,ADD_SHIPPING} from './actionTypes/cartActionType'
+import { ADD_TO_CART,REMOVE_ITEM,SUB_QUANTITY,ADD_QUANTITY} from './actionTypes/cartActionType'
 
 //add cart action
 export const addToCart= (id)=>{
